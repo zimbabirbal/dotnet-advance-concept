d6653112f08643c65b3314f9c600b3a7fb7b374a
zimbabirbal <tamangbirbal23@gmail.com>
2022-06-05 23:37:41 +0545

    Added thread creations sample.

---
+++ Advance-dotnet-concept/C-Sharp Dotnet/Threads/ThreadCreations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_dotnet_concept.C_Sharp_Dotnet.Threads
+{
+    internal class ThreadCreations
+    {
+    }
+}
